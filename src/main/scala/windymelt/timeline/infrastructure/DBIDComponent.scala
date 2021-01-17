73d873513f265bde98ef5d5437d6551151fd35a3
windymelt <windymelt@3qe.us>
2021-01-17 22:13:46 +0900

    DB ID Component

---
+++ src/main/scala/windymelt/timeline/infrastructure/DBIDComponent.scala
@@ -0,0 +1,13 @@
+package windymelt.timeline.infrastructure
+
+import windymelt.timeline.Types
+import windymelt.timeline.domain.repository.IDComponent
+
+trait DBIDComponent extends IDComponent {
+  self: Database =>
+  val ID: IDFactory
+
+  class DBIDFactory extends IDFactory {
+    def gen(): Types.ID = UUID()
+  }
+}
